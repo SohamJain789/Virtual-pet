f85946beaefabe8bc74deb2866dda854e832a035
SohamJain789 <69146549+SohamJain789@users.noreply.github.com>
2020-09-23 08:14:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+//Create variables here
+var dog,dogImg,HappyDogImg,database,foodS,foodStock;
+function preload()
+{
+	//load images here
+	dogImg = loadImage("images/dog.png");
+	HappyDogImg = loadImage("images/dog.png")			
+}
+
+function setup() {
+	createCanvas(500, 500);
+	dog = createSprite(250,250,10,10)
+	dog.addImage(dogImg);
+	dog.scale = 0.5
+	database = firebase.database()
+	foodStock = database.ref('food');
+	foodStock.on("value",readStock);
+
+  
+}
+
+
+function draw() {  
+  background(46, 139, 87)
+  if(keyWentDown(UP_ARROW)){
+  	writeStock(foodS);
+  	dog.addImage(HappyDogImg);
+
+  }
+  drawSprites();
+  //add styles here
+  fill("white")
+  stroke("black")
+  
+  text("Press UP_ARROW key to feed your pet",250,50)
+}
+
+function readStock(data){
+	foodS=data.val();
+}
+
+function writeStock(x){
+	database.ref('/').update({
+		food:x
+	})
+}
+
